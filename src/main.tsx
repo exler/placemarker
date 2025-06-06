0c6ace5c44461148f1beb36eaafa29a5bb3c67c0
Kamil Marut <kamil@kamilmarut.com>
2025-06-06 13:25:35 +0200

    feat: Add TanStack router and TailwindCSS

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,32 @@
+import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
 
-const root = document.getElementById("root");
-if (!root) {
+// Import the generated route tree
+import { routeTree } from "./routeTree.gen";
+
+// Create a new router instance
+const router = createRouter({ routeTree });
+
+// Register the router instance for type safety
+declare module "@tanstack/react-router" {
+    interface Register {
+        router: typeof router;
+    }
+}
+
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
     throw new Error("Root element not found");
 }
 
-createRoot(root).render(
-    <StrictMode>
-        <App />
-    </StrictMode>,
-);
+if (!rootElement.innerHTML) {
+    const root = ReactDOM.createRoot(rootElement);
+    root.render(
+        <StrictMode>
+            <RouterProvider router={router} />
+        </StrictMode>,
+    );
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
+import tailwindcss from "@tailwindcss/vite";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        // Make sure that '@tanstack/router-plugin' is passed before '@vitejs/plugin-react'
+        TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
+        react(),
+        tailwindcss(),
+    ],
 });
